590c711b3ec6414e0471c689decef5599fe797e1
Manish Kumar <manish123hello@gmail.com>
2024-09-19 00:13:38 +0530

    websocket added

--- stream.py
+++ stream.py
@@ -8,3 +8,18 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+with open('measurements.json','r') as file:
+    measurements = iter(json.loads(file.read()))
+    
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.htm", {"request": request})
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        await asyncio.sleep(0.1)
+        payload = next(measurements)
+        await websocket.send_json(payload)
